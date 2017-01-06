c6828259dc3f0223d04847364d706da541c28e38
Solomon Kim <sokim@vassar.edu>
2017-01-06 18:59:26 -0500

    Breakout

--- src/breakout/Breakout.java
+++ src/breakout/Breakout.java
@@ -22,25 +22,6 @@ public class Breakout extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-//        Button btn = new Button();
-//        btn.setText("Say 'Hello World'");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-//            
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("HIIIIIIIIIIIIIII!");
-//                System.out.println("lol u woulda thot!");
-//            }
-//        });
-//        
-//        StackPane root = new StackPane();
-//        root.getChildren().add(btn);
-//        
-//        Scene scene = new Scene(root, 300, 250);
-//        
-//        primaryStage.setTitle("Goodbye Cruel World!");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
             mmg = new MainMenuGUI();
             mmg.initMainMenuGUI(primaryStage);
     }
